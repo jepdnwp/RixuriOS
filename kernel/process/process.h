cf77881172e57b996e51703a9dc79afa0c4b593f
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:33:15 +0300

    process: own address spaces and expose user-process creation API

--- kernel/process/process.h
+++ kernel/process/process.h
@@ -2,6 +2,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "address_space.h"
+
 typedef uint64_t pid_t;
 typedef enum { RIX_PROC_UNUSED=0, RIX_PROC_RUNNING=1, RIX_PROC_SLEEPING=2, RIX_PROC_ZOMBIE=3 } rix_process_state_t;
 #define RIX_PROCESS_MAX 128
@@ -14,8 +16,9 @@ typedef struct {
     rix_process_state_t state;
     uint32_t uid;
     uint32_t gid;
-    uint64_t address_space;
+    rix_address_space_t address_space;
     uint64_t kernel_stack;
+    uint64_t kernel_stack_size;
     uint64_t exit_status;
     uint64_t fd_bitmap;
     char name[RIX_PROCESS_NAME_MAX];
@@ -25,6 +28,8 @@ int process_init(void);
 pid_t process_current(void);
 rix_process_t *process_lookup(pid_t pid);
 int process_create(const char *name, pid_t parent, pid_t *out_pid);
+int process_create_user(const char *name, pid_t parent, const void *image, uint64_t image_size,
+                        uint64_t *out_entry, uint64_t *out_user_stack);
 int process_set_state(pid_t pid, rix_process_state_t state);
 int process_exit(pid_t pid, uint64_t status);
 size_t process_count(void);
