a42f11d5b170a0da8e7ddc410b4debebc429ee9a
hdowkwp <veyselrix@gmail.com>
2026-09-06 01:59:23 +0300

    phase09: support non-owned shared mappings

--- kernel/process/address_space.h
+++ kernel/process/address_space.h
@@ -4,6 +4,7 @@
 typedef struct { uint64_t pml4_phys; } rix_address_space_t;
 int address_space_create(rix_address_space_t *as);
 int address_space_map(rix_address_space_t *as,uint64_t va,uint64_t pa,uint64_t flags);
+int address_space_map_shared(rix_address_space_t *as,uint64_t va,uint64_t pa,uint64_t flags);
 uint64_t address_space_translate(const rix_address_space_t *as,uint64_t va);
 uint64_t address_space_query_flags(const rix_address_space_t *as,uint64_t va);
 void address_space_destroy(rix_address_space_t *as);
