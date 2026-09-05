ac94335c6e033801b8ba0bd8d7aac96334530e27
hdowkwp <veyselrix@gmail.com>
2026-09-06 02:02:47 +0300

    phase10: make DMA API explicitly page-list based

--- kernel/pci/dma.h
+++ kernel/pci/dma.h
@@ -2,7 +2,9 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef struct { uint64_t physical; void *virtual_address; size_t pages; } rix_dma_buffer_t;
+#define RIX_DMA_MAX_PAGES 256
+
+typedef struct { uint64_t pages[RIX_DMA_MAX_PAGES]; size_t count; } rix_dma_buffer_t;
 
 int pci_dma_alloc(size_t pages, uint64_t max_physical_exclusive, rix_dma_buffer_t *out);
 void pci_dma_free(rix_dma_buffer_t *buffer);
