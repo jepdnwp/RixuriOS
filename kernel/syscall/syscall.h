5634db4a7ce25e5bb7f38ed24889154af2f7fafe
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:05:55 +0300

    Define RixuriOS syscall ABI and ring3 trap frame

--- kernel/syscall/syscall.h
+++ kernel/syscall/syscall.h
@@ -1,2 +1,28 @@
 #pragma once
+#include <stdint.h>
+
+#define RIX_SYS_READ       0
+#define RIX_SYS_WRITE      1
+#define RIX_SYS_OPENAT     2
+#define RIX_SYS_CLOSE      3
+#define RIX_SYS_STAT       4
+#define RIX_SYS_GETPID    39
+#define RIX_SYS_EXIT      60
+#define RIX_SYS_WAIT      61
+#define RIX_SYS_MMAP       9
+#define RIX_SYS_MUNMAP    11
+#define RIX_SYS_MPROTECT  10
+#define RIX_SYS_IOCTL     16
+#define RIX_SYS_POLL       7
+#define RIX_SYS_NANOSLEEP 35
+#define RIX_SYSCALL_ABI_VERSION 1u
+
+typedef struct {
+    uint64_t r15,r14,r13,r12,r11,r10,r9,r8;
+    uint64_t rbp,rdi,rsi,rdx,rcx,rbx,rax;
+    uint64_t vector,error;
+    uint64_t rip,cs,rflags,rsp,ss;
+} rix_syscall_frame_t;
+
 void syscall_init(void);
+void syscall_dispatch(rix_syscall_frame_t *frame);
