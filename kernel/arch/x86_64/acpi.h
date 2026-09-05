d46b80505e27ec5b3d8392e700e47cca9780b322
hdowkwp <veyselrix@gmail.com>
2026-09-06 02:00:46 +0300

    phase10: expose ACPI MCFG/ECAM segments

--- kernel/arch/x86_64/acpi.h
+++ kernel/arch/x86_64/acpi.h
@@ -4,9 +4,11 @@
 
 #define ACPI_MAX_CPUS 64
 #define ACPI_MAX_IOAPICS 16
+#define ACPI_MAX_MCFG 8
 
 typedef struct { uint8_t id; uint8_t enabled; uint8_t x2apic; uint32_t apic_id; } acpi_cpu_info_t;
 typedef struct { uint8_t id; uint32_t address; uint32_t gsi_base; } acpi_ioapic_info_t;
+typedef struct { uint64_t base; uint16_t segment; uint8_t start_bus; uint8_t end_bus; } acpi_mcfg_info_t;
 
 int acpi_init(uint64_t rsdp_phys);
 size_t acpi_cpu_count(void);
@@ -14,3 +16,5 @@ size_t acpi_ioapic_count(void);
 const acpi_cpu_info_t *acpi_cpu(size_t index);
 const acpi_ioapic_info_t *acpi_ioapic(size_t index);
 uint32_t acpi_irq_gsi(uint8_t irq, uint16_t *flags);
+size_t acpi_mcfg_count(void);
+const acpi_mcfg_info_t *acpi_mcfg(size_t index);
