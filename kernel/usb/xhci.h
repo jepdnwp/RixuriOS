83f2add7200c4671eca7c1b7dfa24c91ef18dd3b
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:14:07 +0300

    Track xHCI runtime rings and controller state

--- kernel/usb/xhci.h
+++ kernel/usb/xhci.h
@@ -2,7 +2,24 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef struct {uint8_t bus,device,function;uint64_t bar0;uint8_t cap_length;uint8_t max_slots;uint8_t max_intrs;uint8_t max_ports;uint32_t hci_version;uint32_t hcc_params1;uint32_t usbcmd;uint32_t usbsts;} rix_xhci_controller_t;
+typedef struct {
+    uint8_t bus, device, function;
+    uint64_t bar0;
+    uint8_t cap_length;
+    uint8_t max_slots;
+    uint8_t max_intrs;
+    uint8_t max_ports;
+    uint32_t hci_version;
+    uint32_t hcc_params1;
+    uint32_t usbcmd;
+    uint32_t usbsts;
+    uint64_t dcbaa_phys;
+    uint64_t cmd_ring_phys;
+    uint64_t event_ring_phys;
+    uint64_t erst_phys;
+    uint8_t running;
+} rix_xhci_controller_t;
+
 int xhci_init(void);
 size_t xhci_controller_count(void);
 const rix_xhci_controller_t *xhci_controller(size_t index);
