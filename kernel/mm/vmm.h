a1c7631f83e370ef55388761022f10ddf291ce56
hdowkwp <veyselrix@gmail.com>
2026-09-05 16:51:20 +0300

    mm: add virtual memory interface

---
+++ kernel/mm/vmm.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdint.h>
+
+#define RIXURI_PTE_PRESENT  (1ULL << 0)
+#define RIXURI_PTE_WRITE    (1ULL << 1)
+#define RIXURI_PTE_USER     (1ULL << 2)
+#define RIXURI_PTE_NX       (1ULL << 63)
+
+void vmm_early_init(void);
+uint64_t vmm_kernel_pml4(void);
+int vmm_map_page(uint64_t virtual_address, uint64_t physical_address, uint64_t flags);
+void vmm_unmap_page(uint64_t virtual_address);
+uint64_t vmm_translate(uint64_t virtual_address);
