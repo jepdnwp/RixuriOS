74f7fbb0b094e12e54021a951d200e239dc92197
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:00:37 +0300

    Expand scheduler API for real kernel-thread switching

--- kernel/sched/scheduler.h
+++ kernel/sched/scheduler.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <stdint.h>
+
+typedef uint64_t rix_task_id_t;
+typedef void (*rix_kernel_thread_fn)(void *arg);
+
 int scheduler_init(void);
 void scheduler_tick(void);
 uint64_t scheduler_ticks(void);
+int scheduler_create_kernel_thread(rix_kernel_thread_fn entry, void *arg, rix_task_id_t *out_id);
+void scheduler_yield(void);
+rix_task_id_t scheduler_current_id(void);
+uint32_t scheduler_runnable_count(void);
