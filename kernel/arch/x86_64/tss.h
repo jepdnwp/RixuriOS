a51ee7117c6feb5d8fa215f1549090fac523a40f
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:21:49 +0300

    Expose TSS ring3 kernel stack configuration

--- kernel/arch/x86_64/tss.h
+++ kernel/arch/x86_64/tss.h
@@ -1,15 +1,6 @@
 #pragma once
 #include <stdint.h>
-
-typedef struct __attribute__((packed)) {
-    uint32_t reserved0;
-    uint64_t rsp0, rsp1, rsp2;
-    uint64_t reserved1;
-    uint64_t ist[7];
-    uint64_t reserved2;
-    uint16_t reserved3;
-    uint16_t iomap_base;
-} x86_tss_t;
-
+typedef struct __attribute__((packed)){uint32_t reserved0;uint64_t rsp0,rsp1,rsp2;uint64_t reserved1;uint64_t ist[7];uint64_t reserved2;uint16_t reserved3;uint16_t iomap_base;} x86_tss_t;
 void tss_init(void);
+void tss_set_rsp0(uint64_t stack_top);
 const x86_tss_t *tss_current(void);
