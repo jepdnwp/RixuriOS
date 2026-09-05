5ad9b7af76ebcc530530947ce41017fd68a015d1
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:13:24 +0300

    Implement NVMe admin queue and Identify controller API

--- kernel/storage/nvme.h
+++ kernel/storage/nvme.h
@@ -2,7 +2,25 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef struct { uint8_t bus,device,function; uint64_t bar0; uint64_t cap; uint32_t version; uint32_t cc; uint32_t csts; uint16_t mqes; uint8_t dstrd; uint8_t css; } rix_nvme_controller_t;
+typedef struct {
+    uint8_t bus, device, function;
+    uint64_t bar0;
+    uint64_t cap;
+    uint32_t version;
+    uint32_t cc;
+    uint32_t csts;
+    uint16_t mqes;
+    uint8_t dstrd;
+    uint8_t css;
+    uint8_t admin_ready;
+    uint8_t identify_valid;
+    uint32_t serial[5];
+    uint32_t model[10];
+    uint32_t firmware[2];
+    uint32_t nn;
+} rix_nvme_controller_t;
+
 int nvme_init(void);
 size_t nvme_controller_count(void);
 const rix_nvme_controller_t *nvme_controller(size_t index);
+int nvme_identify_controller(size_t index);
