2061aec3b166283a02c217ef2f37f32058aa0320
hdowkwp <veyselrix@gmail.com>
2026-09-05 17:12:08 +0300

    kernel: version boot handoff and expose memory initialization

--- include/kernel.h
+++ include/kernel.h
@@ -1,18 +1,29 @@
 #pragma once
+
 #include <stdint.h>
 #include <stddef.h>
 
 #define RIXURI_BOOT_MAGIC 0x52584955ULL
+#define RIXURI_BOOT_VERSION 1u
 
-typedef struct {
+typedef struct rixuri_boot_info {
     uint64_t magic;
+    uint32_t version;
+    uint32_t size;
     uint64_t memory_map;
     uint64_t memory_map_size;
     uint64_t memory_descriptor_size;
     uint32_t memory_descriptor_version;
+    uint32_t reserved0;
     uint64_t rsdp;
     uint64_t kernel_phys_base;
     uint64_t kernel_phys_end;
+    uint64_t framebuffer_base;
+    uint64_t framebuffer_size;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_format;
 } rixuri_boot_info_t;
 
 void serial_init(void);
