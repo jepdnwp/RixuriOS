f62eb919a68acb1ae64ce165a7c747afd6acf733
hdowkwp <veyselrix@gmail.com>
2026-09-06 02:06:37 +0300

    Phase 09: add process wait/reap ABI

--- kernel/process/process.h
+++ kernel/process/process.h
@@ -10,22 +10,7 @@ typedef enum { RIX_PROC_UNUSED=0, RIX_PROC_RUNNING=1, RIX_PROC_SLEEPING=2, RIX_P
 #define RIX_PROCESS_FD_MAX 64
 #define RIX_SIGNAL_MAX 64
 
-typedef struct {
-    pid_t pid;
-    pid_t parent;
-    rix_process_state_t state;
-    uint32_t uid;
-    uint32_t gid;
-    rix_address_space_t address_space;
-    uint64_t kernel_stack;
-    uint64_t kernel_stack_size;
-    uint64_t exit_status;
-    uint64_t fd_bitmap;
-    uint64_t signal_pending;
-    uint64_t signal_mask;
-    char name[RIX_PROCESS_NAME_MAX];
-} rix_process_t;
-
+typedef struct { pid_t pid; pid_t parent; rix_process_state_t state; uint32_t uid; uint32_t gid; rix_address_space_t address_space; uint64_t kernel_stack; uint64_t kernel_stack_size; uint64_t exit_status; uint64_t fd_bitmap; uint64_t signal_pending; uint64_t signal_mask; char name[RIX_PROCESS_NAME_MAX]; } rix_process_t;
 int process_init(void);
 pid_t process_current(void);
 rix_process_t *process_lookup(pid_t pid);
@@ -34,4 +19,5 @@ int process_create_user(const char *name,pid_t parent,const void *image,uint64_t
 int process_activate(pid_t pid);
 int process_set_state(pid_t pid,rix_process_state_t state);
 int process_exit(pid_t pid,uint64_t status);
+int process_wait(pid_t parent,pid_t wanted,uint64_t *status,pid_t *child_pid);
 size_t process_count(void);
