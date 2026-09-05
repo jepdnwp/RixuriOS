760e40d2efa3f53ba91ebe2b06a17ae73bb17346
hdowkwp <veyselrix@gmail.com>
2026-09-05 18:20:34 +0300

    Connect ELF loader to user address spaces

--- kernel/elf/loader.h
+++ kernel/elf/loader.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include "../process/address_space.h"
 #include "elf.h"
-
-typedef struct { uint64_t entry; uint64_t image_lo; uint64_t image_hi; } rix_elf_image_t;
-int elf_load_image(const void *file,uint64_t file_size,rix_elf_image_t *out);
+typedef struct {uint64_t entry;uint64_t image_lo;uint64_t image_hi;} rix_elf_image_t;
+int elf_load_image(const void *file,uint64_t file_size,rix_address_space_t *as,rix_elf_image_t *out);
