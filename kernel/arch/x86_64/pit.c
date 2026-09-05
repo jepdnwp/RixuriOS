f2bb73467e70c6f7f9db912f4c9e8c3483074ec3
hdowkwp <veyselrix@gmail.com>
2026-09-05 17:54:20 +0300

    Implement PIT timer

---
+++ kernel/arch/x86_64/pit.c
@@ -0,0 +1,9 @@
+#include "pit.h"
+#include "irq.h"
+#include <stdint.h>
+#define PIT_HZ 1193182u
+static volatile uint64_t ticks;
+static inline void outb(uint16_t p,uint8_t v){__asm__ volatile("outb %0,%1"::"a"(v),"Nd"(p));}
+int pit_init(uint32_t hz){if(!hz||hz>PIT_HZ)return -1;uint32_t div=PIT_HZ/hz;if(div<1)div=1;if(div>65535)div=65535;outb(0x43,0x36);outb(0x40,(uint8_t)div);outb(0x40,(uint8_t)(div>>8));ticks=0;return irq_register(0,pit_irq);}
+void pit_irq(unsigned irq,const void *frame){(void)irq;(void)frame;ticks++;}
+uint64_t pit_ticks(void){return ticks;}
