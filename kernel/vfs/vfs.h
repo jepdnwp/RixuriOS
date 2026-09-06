9de7c0a0e299ffcb7e96a94c7144a65335d2ca07
hdowkwp <veyselrix@gmail.com>
2026-09-06 04:22:36 +0300

    vfs: add O_APPEND flag

--- kernel/vfs/vfs.h
+++ kernel/vfs/vfs.h
@@ -10,6 +10,7 @@
 #define RIX_VFS_O_RDWR 2u
 #define RIX_VFS_O_CREAT 4u
 #define RIX_VFS_O_TRUNC 8u
+#define RIX_VFS_O_APPEND 16u
 typedef enum { RIX_VFS_DIR=1, RIX_VFS_FILE=2, RIX_VFS_SYMLINK=3, RIX_VFS_DEVICE=4 } rix_vfs_type_t;
 typedef struct { uint64_t inode; rix_vfs_type_t type; uint32_t mode; uint32_t uid; uint32_t gid; uint64_t size; } rix_vnode_t;
 typedef struct { rix_vnode_t *node; char path[RIX_VFS_PATH_MAX]; } rix_vfs_path_t;
