756cefc6c45e8d369c3874a61a63eb4e1d8e69d6
jepdnwp <2.09807458e+08+jepdnwp@users.noreply.github.com>
2026-09-07 10:07:46 +0000

    Make ls use the current working directory

--- user/programs/ls.c
+++ user/programs/ls.c
@@ -6,7 +6,7 @@ static void out(const char *s) { (void)write(1,s,length(s)); }
 
 int program_main(int argc, char **argv, char **envp) {
     (void)envp;
-    const char *path = argc > 1 ? argv[1] : "/";
+    const char *path = argc > 1 ? argv[1] : ".";
     int fd = openat(-100, path, 0, 0);
     if (fd < 0) { out("ls: open failed\n"); return 1; }
     rix_dirent_t entries[16]; size_t count=0;
